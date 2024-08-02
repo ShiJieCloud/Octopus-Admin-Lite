5f4ecc584a863724fa3c0e7c942ac2155b17c9c3
赵士杰 <1500492856@qq.com>
2024-08-02 20:35:27 +0800

    feat:清除浏览器默认样式

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,9 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 
+// 全局样式
+import './assets/styles/index.scss'
+
 const app = createApp(App)
 
 app.use(createPinia())
