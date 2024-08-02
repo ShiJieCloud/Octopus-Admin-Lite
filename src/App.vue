02f00b675e8ce3fb1add76c7aaf9159dcbfce02c
赵士杰 <1500492856@qq.com>
2024-08-02 20:04:27 +0800

    feat:集成ElementPlus组件库

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,14 @@ const title = import.meta.env.VITE_APP_TITLE
 
 <template>
   <h1>{{ title }}</h1>
+  <div class="mb-4">
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+  </div>
 </template>
 
 <style scoped>
