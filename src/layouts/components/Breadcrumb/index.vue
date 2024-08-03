cdd98c991e69f272d6784488a01296a4462bc29c
赵士杰 <1500492856@qq.com>
2024-08-03 19:28:21 +0800

    feat:添加样式使面包屑文本不换行

--- src/layouts/components/Breadcrumb/index.vue
+++ src/layouts/components/Breadcrumb/index.vue
@@ -3,14 +3,19 @@
 </script>
 
 <template>
-  <el-breadcrumb separator="/">
+  <el-breadcrumb separator="/" class="breadcrumb-container">
     <el-breadcrumb-item :to="{ path: '/' }">homepage</el-breadcrumb-item>
-    <el-breadcrumb-item>promotion management</el-breadcrumb-item>
-    <el-breadcrumb-item>promotion list</el-breadcrumb-item>
-    <el-breadcrumb-item>promotion detail</el-breadcrumb-item>
+    <el-breadcrumb-item>management</el-breadcrumb-item>
+    <el-breadcrumb-item>list</el-breadcrumb-item>
+    <el-breadcrumb-item>detail</el-breadcrumb-item>
   </el-breadcrumb>
 </template>
 
-<style scoped lang="scss">
-
+<style scoped lang="postcss">
+.breadcrumb-container {
+  display: flex;
+  flex-wrap: nowrap; /* 防止换行 */
+  overflow: hidden; /* 横向滚动溢出处理 */
+  padding-right: 12px;
+}
 </style>
