60a92297c9f9b186e2d33a216159b716e38fe5d4
赵士杰 <1500492856@qq.com>
2024-08-03 19:11:18 +0800

    feat:添加左侧菜单组件

--- src/layouts/components/Menu/index.vue
+++ src/layouts/components/Menu/index.vue
@@ -1,11 +1,64 @@
 <script setup lang="ts">
+const handleOpen = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
+const handleClose = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
+
+const props = defineProps({
+  mode: {
+    type: String,
+    default: 'vertical'
+  }
+})
 
 </script>
-
+<!--vertical horizontal-->
 <template>
-  <div>Menu</div>
+  <div class="menu-container">
+    <el-menu
+      default-active="2"
+      class="os-menu"
+      @open="handleOpen"
+      @close="handleClose"
+      :mode="props.mode"
+    >
+      <el-sub-menu index="1">
+        <template #title>
+          <span>Navigator One</span>
+        </template>
+        <el-menu-item-group title="Group One">
+          <el-menu-item index="1-1">item one</el-menu-item>
+          <el-menu-item index="1-2">item two</el-menu-item>
+        </el-menu-item-group>
+        <el-menu-item-group title="Group Two">
+          <el-menu-item index="1-3">item three</el-menu-item>
+        </el-menu-item-group>
+        <el-sub-menu index="1-4">
+          <template #title>item four</template>
+          <el-menu-item index="1-4-1">item one</el-menu-item>
+        </el-sub-menu>
+      </el-sub-menu>
+      <el-menu-item index="2">
+        <span>Navigator Two</span>
+      </el-menu-item>
+      <el-menu-item index="3" disabled>
+        <span>Navigator Three</span>
+      </el-menu-item>
+      <el-menu-item index="4">
+        <span>Navigator Four</span>
+      </el-menu-item>
+    </el-menu>
+  </div>
 </template>
 
-<style scoped lang="scss">
+<style scoped lang="postcss">
+.menu-container{
+  background-color: var(--el-menu-bg-color);
 
+  .os-menu{
+    @apply border-0;
+  }
+}
 </style>
