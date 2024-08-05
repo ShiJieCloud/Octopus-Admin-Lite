3b37f27f716eff395f8b8d8744f244afc9c49603
赵士杰 <1500492856@qq.com>
2024-08-05 09:52:15 +0800

    feat:修复Layout布局组件

--- src/layouts/index.vue
+++ src/layouts/index.vue
@@ -1,13 +1,12 @@
 <script setup lang="ts">
 import { type Component, computed } from 'vue'
 import LayoutClassic from '@/layouts/LayoutClassic/index.vue'
-import LayoutTransverse from '@/layouts/LayoutHorizontal/index.vue'
-import Theme from '@/layouts/components/Theme/index.vue'
+import LayoutHorizontal from '@/layouts/LayoutHorizontal/index.vue'
 
 // 定义布局模式与对应的组件类型
 const layoutMode: Record<string, Component> = {
   classic: LayoutClassic,   // 经典布局
-  horizontal: LayoutTransverse  // 横向布局
+  horizontal: LayoutHorizontal  // 横向布局
 }
 
 // 计算属性，根据具体逻辑返回所选布局的组件
@@ -15,9 +14,8 @@ const mode = computed(() => layoutMode['classic'])
 </script>
 
 <template>
-  <!-- 根据 layout 的值渲染不同的布局样式 -->
+  <!-- 根据 mode 的值渲染不同的布局样式 -->
   <component :is="mode" />
-  <Theme />
 </template>
 
 <style scoped lang="postcss">
