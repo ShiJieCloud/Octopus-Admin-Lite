1124b46926bcf2611e8e86914982d80bc7b588d9
赵士杰 <1500492856@qq.com>
2024-08-24 14:41:19 +0800

    feat:Menu添加分页

--- src/views/menu/index.vue
+++ src/views/menu/index.vue
@@ -1,16 +1,33 @@
 <script setup lang="ts" name="Menu">
 
-import { onMounted, reactive, ref } from 'vue'
+import { onBeforeUnmount, onMounted, reactive, ref, watch } from 'vue'
 import { getMenuListApi } from '@/api/interface/menu'
 import { ElMessage } from 'element-plus'
+import { useThemeStore } from '@/stores/modules/theme'
+
+const { themeConfig } = useThemeStore()
+
+// 菜单数据
+const menuData = ref()
 
 // 搜索表单
-const searchFormLoading = ref(false)
+const formContainerRef = ref<HTMLElement | null>(null)
 const searchForm = reactive({
   title: '',
   status: null,
   startDate: '',
   endDate: ''
+})
+
+// 表格高度
+const tableHeight = ref<number>(0)
+
+// 分页表单
+const pageForm = reactive({
+  pageNum: 1,
+  pageSize: 10,
+  currentPage: 1,
+  total: 0
 })
 
 // 重置搜索表单
@@ -35,25 +52,15 @@ const endDateDisabledDate = (date: Date) => {
   return date.getTime() < new Date(startDate).getTime()
 }
 
-// 菜单数据
-const menuData = ref()
-
 // 搜索菜单
 const searchMenu = async () => {
   // 调用 getMenuListApi
-  searchFormLoading.value=true
   menuData.value = await getMenuListApi(searchForm)
-  searchFormLoading.value = false
+  pageForm.total = menuData.value.length
 }
 
-onMounted(() => {
-  searchMenu()
-})
-
 // 筛选菜单状态
-const filterStatus = (value: number, row) => {
-  console.log(value)
-  console.log(row.status)
+const filterStatus = (value: number, row: any) => {
   return row.status === value
 }
 
@@ -64,6 +71,44 @@ const handleDeleteMenu = (menuId: any) => {
     message: '删除成功',
     type: 'success'
   })
+}
+
+// 如果屏幕小于1024，tableHeight - formHeight - 100；否则tableHeight - formHeight - 345
+const updateTableHeight = () => {
+  if (formContainerRef.value) {
+    const formHeight = formContainerRef.value.offsetHeight
+    const middleScreen = window.innerWidth < 1024
+    let padding = middleScreen ? 285 : 350
+    if (!themeConfig.showTabs && !middleScreen) {
+      padding -= 40
+    }
+    tableHeight.value = window.innerHeight - formHeight - padding
+  }
+}
+
+watch(formContainerRef, updateTableHeight)
+
+watch(() => themeConfig.showTabs, updateTableHeight)
+
+onMounted(() => {
+  searchMenu()
+  updateTableHeight()
+  window.addEventListener('resize', updateTableHeight)
+})
+
+onBeforeUnmount(() => {
+  window.removeEventListener('resize', updateTableHeight)
+})
+
+// 切换页
+const handleUpdatePage = (pageNum: number) => {
+  pageForm.pageNum = pageNum
+  pageForm.currentPage = pageNum
+}
+
+// 修改页大小
+const handleUpdatePageSize = (size: number) => {
+  pageForm.pageSize = size
 }
 </script>
 
@@ -87,7 +132,7 @@ const handleDeleteMenu = (menuId: any) => {
                   </el-select>
                 </el-form-item>
               </el-col>
-              <el-col :xs="24" :sm="18" :lg="10">
+              <el-col :xs="24" :sm="16" :lg="9">
                 <el-form-item label="创建日期">
                   <el-col :span="11">
                     <el-date-picker
@@ -112,12 +157,14 @@ const handleDeleteMenu = (menuId: any) => {
                   </el-col>
                 </el-form-item>
               </el-col>
-              <el-col :xs="24" :sm="6" :lg="4">
+              <el-col :xs="24" :sm="8" :lg="5">
                 <el-form-item class="float-right">
-                  <el-button type="primary" @click="searchMenu" :loading="searchFormLoading">
-                    <svg-icon name="Search" class="mr-1" /> 查询 </el-button>
-                  <el-button @click="resetSearchForm">
-                    <svg-icon name="reset" class="mr-1" /> 重置 </el-button>
+                  <el-space>
+                    <el-button type="primary" @click="searchMenu">
+                      <svg-icon name="Search" class="mr-1" /> 查询 </el-button>
+                    <el-button @click="resetSearchForm">
+                      <svg-icon name="reset" class="mr-1" /> 重置 </el-button>
+                  </el-space>
                 </el-form-item>
               </el-col>
             </el-row>
@@ -126,11 +173,11 @@ const handleDeleteMenu = (menuId: any) => {
       </div>
       <el-card>
         <template #header>
-          <div>
+          <el-space wrap>
             <el-button type="primary" plain><svg-icon size="18px" name="add" class="mr-1" /> 新增菜单 </el-button>
             <el-button type="danger" plain><svg-icon size="18px" name="delete" class="mr-1" /> 批量删除 </el-button>
             <el-button plain><svg-icon size="18px" name="expand-fold" class="mr-1" /> 展开/折叠 </el-button>
-          </div>
+          </el-space>
         </template>
         <el-table
           ref="menuTableRef"
@@ -138,7 +185,7 @@ const handleDeleteMenu = (menuId: any) => {
           highlight-current-row
           :data="menuData"
           table-layout="auto"
-
+          :height="tableHeight"
         >
           <el-table-column type="selection" width="55" />
           <el-table-column fixed prop="title" label="菜单" />
@@ -173,16 +220,16 @@ const handleDeleteMenu = (menuId: any) => {
           <el-table-column prop="createTime" label="创建时间" min-width="180" header-align="center" align="center" />
           <el-table-column label="操作" min-width="200" header-align="center" align="center" >
             <template #default="scope">
-              <el-button link size="small" type="primary"><svg-icon size="16px" name="edit" class="mr-1" /> 查看详情 </el-button>
-              <el-button link size="small" type="primary"><svg-icon size="16px" name="add" class="mr-1" /> 新增 </el-button>
-              <el-popconfirm
-                title="确定要删除吗?"
-                @confirm="handleDeleteMenu(scope.row.id)"
-              >
-                <template #reference>
-                  <el-button link size="small" type="danger"><svg-icon size="16px" name="delete" class="mr-1" /> 删除 </el-button>
-                </template>
-              </el-popconfirm>
+                <el-button link size="small" type="primary"><svg-icon size="16px" name="edit" class="mr-1" /> 查看详情 </el-button>
+                <el-button link size="small" type="primary"><svg-icon size="16px" name="add" class="mr-1" /> 新增 </el-button>
+                <el-popconfirm
+                  title="确定要删除吗?"
+                  @confirm="handleDeleteMenu(scope.row.id)"
+                >
+                  <template #reference>
+                    <el-button link size="small" type="danger"><svg-icon size="16px" name="delete" class="mr-1" /> 删除 </el-button>
+                  </template>
+                </el-popconfirm>
             </template>
           </el-table-column>
 
@@ -193,7 +240,13 @@ const handleDeleteMenu = (menuId: any) => {
           </template>
         </el-table>
         <template #footer>
-
+          <Pagination
+            :page="pageForm.currentPage"
+            :size="pageForm.pageSize"
+            :total="pageForm.total"
+            @update:page="handleUpdatePage"
+            @update:size="handleUpdatePageSize"
+          />
         </template>
       </el-card>
     </div>
