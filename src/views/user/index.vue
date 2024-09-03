c77083381c582127df3497b102f4b10961480570
赵士杰 <1500492856@qq.com>
2024-09-03 21:21:02 +0800

    feat:补充用户管理页面

--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -4,6 +4,7 @@ import { onBeforeUnmount, onMounted, reactive, ref, watch } from 'vue'
 import { getDepartmentListApi } from '@/api/interface/department'
 import { useThemeStore } from '@/stores/modules/theme'
 import { getUserListApi } from '@/api/interface/user'
+import { getRoleListApi } from '@/api/interface/role'
 
 const { themeConfig } = useThemeStore()
 
@@ -12,6 +13,8 @@ const filterDepartmentText = ref()
 const departmentTreeRef = ref()
 
 const departmentData = ref()
+
+const roleData = ref()
 
 const departmentTreeProps = {
   children: 'children',
@@ -44,8 +47,57 @@ const pageForm = reactive({
   total: 0
 })
 
+const userDialogConfig = reactive({
+  visible: false,
+  title: '新增用户',
+  width: 600
+})
+
+const defaultUserForm = {
+  id: null,
+  name: '',
+  phone: '',
+  email: '',
+  avatar: '',
+  gender: null,
+  departmentId: null,
+  positionId: null,
+  roles: [],
+  status: true,
+  remark: ''
+}
+
+const userForm = ref({ ...defaultUserForm })
+
+// 重置表单的方法
+const resetUserForm = () => {
+  userForm.value = { ...defaultUserForm }
+}
+
+// 处理对话框关闭事件
+const handleBeforeClose = (done) => {
+  resetUserForm()
+  done()
+}
+
+const handleAddUser = () => {
+  userDialogConfig.visible = true
+  userDialogConfig.title = '新增用户'
+}
+
+const handleUpdateUser = (row: any) => {
+  userForm.value = { ...row }
+  userDialogConfig.visible = true
+  userDialogConfig.title = '编辑用户'
+}
+
 const getUserData = async () => {
   userData.value = await getUserListApi()
+}
+
+const getRoleData = async () => {
+  roleData.value = await getRoleListApi(filterDepartmentText)
+  console.log(roleData.value)
 }
 
 // 获取部门数据
@@ -56,6 +108,12 @@ const getDepartmentData = async () => {
 const filterDepartment = (value: string, data: any) => {
   if (!value) return true
   return data.name.includes(value)
+}
+
+if (window.innerWidth < 678) {
+  userDialogConfig.width = 350
+} else {
+  userDialogConfig.width = 600
 }
 
 // 如果屏幕小于1024，tableHeight - formHeight - 100；否则tableHeight - formHeight - 345
@@ -84,6 +142,7 @@ watch(tableHeaderButRef, updateTableHeight)
 watch(() => themeConfig.showTabs, updateTableHeight)
 
 onMounted(() => {
+  getRoleData()
   getUserData()
   getDepartmentData()
   updateTableHeight()
@@ -159,7 +218,7 @@ onBeforeUnmount(() => {
           <template #header>
             <div ref="tableHeaderButRef">
               <el-space wrap>
-                <el-button type="primary" plain>
+                <el-button type="primary" plain @click="handleAddUser">
                   <svg-icon size="18px" name="add" class="mr-1" />
                   新增
                 </el-button>
@@ -180,20 +239,20 @@ onBeforeUnmount(() => {
           >
             <el-table-column type="selection" width="50" />
             <el-table-column prop="name" width="120" label="用户" />
-            <el-table-column prop="avatar" width="100" class-name="item-center" label="头像" >
+            <el-table-column prop="avatar" width="100" class-name="item-center" label="头像">
               <template #default="scope">
                 <div class="flex items-center w-7 h-7 rounded-full overflow-hidden">
-                  <el-image :src="scope.row.avatar" preview-teleported :preview-src-list="[scope.row.avatar]"/>
+                  <el-image :src="scope.row.avatar" preview-teleported :preview-src-list="[scope.row.avatar]" />
                 </div>
               </template>
             </el-table-column>
             <el-table-column prop="gender" width="100" label="性别">
               <template #default="scope">
-                <el-tag v-if="scope.row.gender != null" type="primary">{{ scope.row.gender }}</el-tag>
+                <el-tag v-if="scope.row.gender != null" type="primary">{{ scope.row.gender == 1 ? '男' : '女' }}</el-tag>
               </template>
             </el-table-column>
             <el-table-column prop="phone" width="150" label="手机号码" />
-            <el-table-column prop="email" width="150" label="邮箱" />
+            <el-table-column prop="email" width="250" label="邮箱" />
             <el-table-column
               prop="status"
               label="状态"
@@ -212,26 +271,38 @@ onBeforeUnmount(() => {
                 />
               </template>
             </el-table-column>
-            <el-table-column prop="createTime" label="注册时间" min-width="150" header-align="center" align="center" />
-            <el-table-column label="操作" fixed="right" width="150" header-align="center" align="center">
+            <el-table-column prop="createTime" show-overflow-tooltip label="注册时间" min-width="150" header-align="center" align="center" />
+            <el-table-column label="操作" fixed="right" width="180" header-align="center" align="center">
               <template #default="scope">
-                <el-button link size="small" type="primary">
-                  <svg-icon size="16px" name="edit" class="mr-1" />
-                  修改
-                </el-button>
-                <el-popconfirm
-                  title="确定要删除吗?"
-                >
-                  <template #reference>
-                    <el-button link size="small" type="danger">
-                      <svg-icon size="16px" name="delete" class="mr-1" />
-                      删除
+                <div class="flex items-center gap-3">
+                  <div>
+                    <el-button link size="small" type="primary" @click="handleUpdateUser(scope.row)">
+                      <svg-icon size="16px" name="edit" class="mr-1" />
+                      修改
                     </el-button>
-                  </template>
-                </el-popconfirm>
+                  </div>
+                  <el-popconfirm
+                    title="确定要删除吗?"
+                  >
+                    <template #reference>
+                      <el-button link size="small" type="danger">
+                        <svg-icon size="16px" name="delete" class="mr-1 ml-0" />
+                        删除
+                      </el-button>
+                    </template>
+                  </el-popconfirm>
+                  <el-dropdown>
+                    <svg-icon size="16px" name="more" />
+                    <template #dropdown>
+                      <el-dropdown-menu>
+                        <el-dropdown-item>上传头像</el-dropdown-item>
+                        <el-dropdown-item>重置密码</el-dropdown-item>
+                      </el-dropdown-menu>
+                    </template>
+                  </el-dropdown>
+                </div>
               </template>
             </el-table-column>
-
             <template #empty>
               <div class="flex items-center justify-center h-100%">
                 <el-empty />
@@ -248,6 +319,83 @@ onBeforeUnmount(() => {
         </el-card>
       </div>
     </div>
+    <el-dialog
+      v-model="userDialogConfig.visible"
+      :title="userDialogConfig.title"
+      :width="userDialogConfig.width"
+      :before-close="handleBeforeClose"
+    >
+      <div>
+        <el-form :model="userForm" label-width="auto">
+          <el-row :gutter="12">
+            <el-col :xs="24" :sm="12">
+              <el-form-item label="用户名">
+                <el-input v-model="userForm.name" />
+              </el-form-item>
+            </el-col>
+            <el-col :xs="24" :sm="12">
+              <el-form-item label="手机号码">
+                <el-input v-model="userForm.phone" />
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row :gutter="12">
+            <el-col :xs="24" :sm="12">
+              <el-form-item label="邮箱">
+                <el-input v-model="userForm.email" />
+              </el-form-item>
+            </el-col>
+            <el-col :xs="24" :sm="12">
+              <el-form-item label="性别">
+                <el-radio-group v-model="userForm.gender">
+                  <el-radio :value="1">男</el-radio>
+                  <el-radio :value="0">女</el-radio>
+                </el-radio-group>
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-form-item label="所属部门">
+            <el-tree-select
+              v-model="userForm.departmentId"
+              :highlight-current="true"
+              :data="departmentData"
+              :props="departmentTreeProps"
+              :render-after-expand="false"
+            />
+          </el-form-item>
+          <el-form-item label="角色权限">
+            <el-select
+              v-model="userForm.roles"
+              placeholder="请选择角色"
+              clearable
+              multiple
+              collapse-tags
+              collapse-tags-tooltip
+              :max-collapse-tags="3"
+            >
+              <el-option
+                v-for="item in roleData"
+                :key="item.id"
+                :label="item.name"
+                :value="item.id"
+              />
+            </el-select>
+          </el-form-item>
+          <el-form-item label="备注">
+            <el-input v-model="userForm.remark" type="textarea" />
+          </el-form-item>
+          <el-form-item label="状态">
+            <el-switch v-model="userForm.status" />
+          </el-form-item>
+        </el-form>
+      </div>
+      <template #footer>
+        <div style="flex: auto">
+          <el-button @click="userDialogConfig.visible = false">取消</el-button>
+          <el-button type="primary" @click="userDialogConfig.visible = false"> 确认</el-button>
+        </div>
+      </template>
+    </el-dialog>
   </div>
 </template>
 
@@ -288,6 +436,10 @@ onBeforeUnmount(() => {
       background-color: var(--el-bg-color);
     }
   }
+}
+
+:deep(.el-form-item__content) {
+  @apply flex;
 }
 
 :deep(.search-from .el-card__body) {
